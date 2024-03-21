c18b5af891ad4116969e985499e61b30298c7e55
Katsuyuki Omuro <komuro@fastly.com>
2024-03-22 00:47:28 +0900

    Simplify SSR

--- src/ssr.ts
+++ src/ssr.ts
@@ -1,49 +1,27 @@
-import React, { type ReactElement } from "react";
 import ReactDOMServer from "react-dom/server";
 import ReactServerDOMClient from "react-server-dom-webpack/client";
 
-// TODO: This declaration will no longer be needed when React exposes the definition
-declare module "React" {
-  function use<T>(obj: Promise<T>): T;
-}
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
 
-type FlightContent = { root: ReactElement };
-
-// * SSR *
-// "Shell" component used during SSR
-// The 'use' hook works with ReactDOMServer.renderToReadableStream,
-// waiting for the promise to resolve to the React root
-function Shell(props: {flightContentPromise: Promise<FlightContent>}): any {
-  return React.use(props.flightContentPromise).root;
-}
+const FLIGHT_SCRIPT_TAG_PREAMBLE = encoder.encode('<script id="react-flight-data" type="react/flight">');
+const FLIGHT_SCRIPT_TAG_POSTAMBLE = encoder.encode('</script>');
 
 // * SSR *
-// Renders the flight stream into HTML
+// Renders the flight stream into HTML, and also injects
+// a string representation of the flight stream as a script tag
 export async function renderFlightStreamToHtmlStream(
-  flightStream: ReadableStream<Uint8Array>
+  flightStream: ReadableStream<Uint8Array>,
 ) {
-  // This procedure is borrowed form React's flight fixture.
 
-  // We need to get the formState before we start rendering, but we also
-  // need to run the Flight client inside the render to get all the preloads.
-  // The API is ambivalent about what's the right one, so we need two for now.
+  // TODO: When @fastly/js-compute implements ReadableStream.prototype.tee(),
+  // we can use it to save buffering the entire flight stream to memory
+  const flightStreamBuffer = await new Response(flightStream).arrayBuffer();
+  flightStream = new Response(flightStreamBuffer).body!;
 
-  // Tee the response into two streams so that we can do both.
-  // TODO: use .tee() when Compute runtime supports it
-  // const [ rscResponse1, rscResponse2 ] = flightStream.tee();
-  const [ rscResponse1, rscResponse2 ] = await teeReadableStream(flightStream);
-
-  const { formState } = await ReactServerDOMClient.createFromReadableStream(rscResponse1);
-
-  // This is equivalent to the following JSX, but we're not using JSX in this file:
-  // <Shell flightContentPromise={/* createFromReadableStream */} />
-  const model = React.createElement(Shell, {
-    flightContentPromise: ReactServerDOMClient.createFromReadableStream(rscResponse2),
-  });
-
-  // Render it into HTML by resolving the client components
-  return await ReactDOMServer.renderToReadableStream(
-    model,
+  const { formState, root } = await ReactServerDOMClient.createFromReadableStream(flightStream);
+  const htmlStream: ReadableStream<Uint8Array> = await ReactDOMServer.renderToReadableStream(
+    root,
     {
       bootstrapScripts: [
         '/app/runtime.js',
@@ -52,116 +30,55 @@ export async function renderFlightStreamToHtmlStream(
       formState,
     }
   );
-}
 
-// * SSR *
-// A utility "transform stream" that is used to inject a copy of the flight stream into
-// the HTML stream as a script tag.
-class FlightStreamInjectionTransform extends TransformStream<Uint8Array, Uint8Array> {
-  static decoder = new TextDecoder();
-  static encoder = new TextEncoder();
-  constructor(flightStream: ReadableStream<Uint8Array>) {
-    let alreadyInjected = false;
+  // Use a TransformStream to inject a script tag that contains the string representation of
+  // the flight stream into the output HTML stream.
+  let alreadyInjected = false;
+  const flightStreamChunks: Uint8Array[] = [
+    FLIGHT_SCRIPT_TAG_PREAMBLE,
+    new Uint8Array(flightStreamBuffer),
+    FLIGHT_SCRIPT_TAG_POSTAMBLE,
+  ];
 
-    const transformer = {
-      async transform(
-        chunk: Uint8Array,
-        controller: TransformStreamDefaultController<Uint8Array>,
-      ) {
-        if (!((chunk as unknown) instanceof Uint8Array)) {
-          // Guard anyway in case someone uses this TransformStream with an unexpected stream type
-          throw new Error('Received non-Uint8Array chunk');
-        }
-        if (alreadyInjected) {
-          controller.enqueue(chunk);
-          return;
-        }
-
-        const textChunk = FlightStreamInjectionTransform.decoder.decode(chunk);
-        const closingHeadPos = textChunk.indexOf('</head>');
-        if (closingHeadPos === -1) {
-          controller.enqueue(chunk);
-          return;
-        }
-
-        function enqueueString(stringChunk: string) {
-          controller.enqueue(FlightStreamInjectionTransform.encoder.encode(stringChunk));
-        }
-        enqueueString(
-          textChunk.slice(0, closingHeadPos)
-        );
-        enqueueString(
-          `<script id="react-flight-data" type="react/flight">`
-        );
-
-        // TODO: Use an async iterator when Compute runtime gets support
-        // for await (const value of flightStream)
-        const reader = flightStream.getReader();
-        while(true) {
-          const {done, value} = await reader.read();
-          if (done) {
-            break;
-          }
-          controller.enqueue(value);
-        }
-
-        enqueueString(
-          `</script>`
-        );
-        enqueueString(
-          textChunk.slice(closingHeadPos)
-        );
-        alreadyInjected = true;
-      },
-    };
-    super(transformer);
-  }
-}
-
-// * SSR *
-// Inject the flight stream into the HTML stream as a script tag.
-export function injectFlightStreamIntoRenderStream(
-  renderStream: ReadableStream<Uint8Array>,
-  flightStream: ReadableStream<Uint8Array>,
-): ReadableStream<Uint8Array> {
-  return renderStream.pipeThrough(new FlightStreamInjectionTransform(flightStream));
-}
-
-// * SSR *
-// A combined utility version that renders the flight stream to HTML and
-// then also injects the flight stream into the HTML stream as a script tag.
-export async function renderFlightStreamToHtmlStreamWithFlightData(
-  flightStream: ReadableStream<Uint8Array>,
-) {
-
-  // The flight stream is needed twice:
-  // - Once now to render HTML (purpose A)
-  // - Later in the client during app hydration (purpose B)
-
-  // After rendering the HTML (purpose A), we will inject a copy of the flight
-  // stream into the HTML body so that the client side code can use it
-  // for hydration (purpose B). If we didn't do this, then the client side code would have
-  // to make an additional fetch to perform hydration.
-
-  // Because a ReadableStream can only be streamed from once, we tee it.
-  // TODO: use .tee() when Compute runtime supports it
-  // const [ flightStream1, flightStream2 ] = flightStream.tee();
-  const [ flightStream1, flightStream2 ] = await teeReadableStream(flightStream);
-
-  // Render the flight stream to HTML (purpose A)
-  const renderStream = await renderFlightStreamToHtmlStream(flightStream1);
-
-  // Inject the flight stream data into the HTML stream as a script tag (purpose B)
-  return injectFlightStreamIntoRenderStream(renderStream, flightStream2);
-
-}
-
-async function teeReadableStream(stream: ReadableStream<Uint8Array>) {
-  return new Promise<[ReadableStream<Uint8Array>, ReadableStream<Uint8Array>]>(async resolve => {
-    const content = await new Response(stream).arrayBuffer();
-    resolve([
-      new Response(content).body!,
-      new Response(content).body!,
-    ]);
+  const flightStreamInjectionTransform = new TransformStream<Uint8Array, Uint8Array>({
+    async transform(
+      chunk,
+      controller,
+    ) {
+      if (alreadyInjected) {
+        controller.enqueue(chunk);
+        return;
+      }
+      // It's inserted right before the closing </body> tag in the HTML stream.
+      const textChunk = decoder.decode(chunk);
+      const closingBodyPos = textChunk.indexOf('</body>');
+      if (closingBodyPos === -1) {
+        controller.enqueue(chunk);
+        return;
+      }
+      const chunks = [
+        encoder.encode(textChunk.slice(0, closingBodyPos)),
+        ...flightStreamChunks,
+        encoder.encode(textChunk.slice(closingBodyPos)),
+      ];
+      for (const chunk of chunks) {
+        controller.enqueue(chunk);
+      }
+      alreadyInjected = true;
+    },
+    flush(
+      controller,
+    ) {
+      // If we've reached the end of the input stream and there hasn't been a closing
+      // </body> tag for some reason, then insert it at the very end.
+      if (alreadyInjected) {
+        return;
+      }
+      for (const chunk of flightStreamChunks) {
+        controller.enqueue(chunk);
+      }
+    },
   });
+
+  return htmlStream.pipeThrough(flightStreamInjectionTransform);
 }
